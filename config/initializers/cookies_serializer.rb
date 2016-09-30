e9f8322b4141154bc2ac914f8acf16bded6500af
michelegozzi <michelegozzi@gmail.com>
2016-09-30 16:49:40 -0700

    secret key base initializer added

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
+Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
 Rails.application.config.action_dispatch.cookies_serializer = :json
